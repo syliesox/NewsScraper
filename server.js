4c475e992feff6bf60ea37cde3896f80894233d5
syliesox <syliesox@gmail.com>
2019-07-02 20:24:49 -0400

    fixed scraping tags for articles

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ app.get("/scrape", function(req, res) {
       var $ = cheerio.load(response.data);
   
       // Now, we grab every h2 within an article tag, and do the following:
-      $("article").each(function(i, element) {
+      $("article div div").each(function(i, element) {
         // Save an empty result object
         var result = {};
   
@@ -41,10 +41,10 @@ app.get("/scrape", function(req, res) {
         result.title = $(this)
           .children("a")
           .text();
-        result.link = $(this)
+        result.link = "https://www.nytimes.com" + $(this)
           .children("a")
           .attr("href");
-  
+
         // Create a new Article using the `result` object built from scraping
         db.Article.create(result)
           .then(function(dbArticle) {
